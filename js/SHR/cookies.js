8e190d8671ed5f5d1091c205a3043786f735fbfd
Bedaze <daniel.barnes@students.mq.edu.au>
2016-09-25 20:46:41 +1000

    JS file update

--- js/SHR/cookies.js
+++ js/SHR/cookies.js
@@ -6,63 +6,33 @@ function SETcookie() {
 }
 
 function GETcookie() {
-  if (document.cookie){
-    eval(document.cookie);
+  var cookieName = selectId + "Selected=";
 
-  selectElement.selectedIndex = eval(selectId + Selected);
-}}
+  var splitCookie = document.cookie.split(';');
 
-function Save_Items() {
-  SaveString = SelectId + 'Items="';
-
-  for (i = 0; i < selectElement.length; ++i)
-    SaveString += selectElement.options[i].innerText + ',' + selectElement.options[i].value + ',';
-
-  SaveString = SaveString.substring(0, SaveString.length-1) + '"';
-  document.cookie = SaveString;
-}
-
-function Clear_Items() {
-  while (selectElement.length > 0) {
-    selectElement.remove(0);
+  for(i = 0; i < splitCookie.length; ++i) {
+    var c = splitCookie[i];
+    
+    while (c.charAt(0) == ' ')
+      c = c.substring(1);
+    
+    if (c.indexOf(cookieName) == 0)
+      selectElement.selectedIndex = c.substring(cookieName.length, c.length);
   }
 }
 
-function Restore_Item(IT,VA) {
-  var oOption = document.createElement("option");
-  selectElement.options.add(oOption);
-  oOption.innerText = IT;
-  oOption.value = VA;
-}
 
-function Restore_Select() {
- // remove items from select
- if (document.cookie)
-    Clear_Items();
-
- eval(document.cookie);
-
- Items = document.cookie;
-
- Data = Items.split(',');
- for (x = 0; x < Data.length-1; x += 2) {
-  Restore_Item(Data[x], Data[x+1]);
- }
-}
 
 window.onload = function() {
   selectElement = document.getElementsByClassName('cookieSelect')[0];
   selectId = selectElement.id;
 
-  //GETcookie();
+  GETcookie();
 
-  //Restore_Select();
+
 
   selectElement.onchange = function () {
     SETcookie();
   }
 
-  /*selectElement.onclick = function () {
-    Save_Items();
-  }*/
 }
