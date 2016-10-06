87cd1cdedf6df1ebf6cf55747559b47c4b41b1f3
Tim Bishop <Tim Bishop>
2016-10-06 19:11:09 +1100

    fixed dbl quotation marks

--- SHRDS/js/views/LogonView.js
+++ SHRDS/js/views/LogonView.js
@@ -10,19 +10,19 @@ var LogonView = function(){
 		var body = 
 		"<div class='radioButtons'>"+
 		"<input type='radio' name='radio' id='radio1' class="radio" value="checked" >"+
-		"<label for="radio1">TSO</label>"+
-		"<input type="radio" name="radio" id="radio2" class="radio"/>"+
-		"<label for="radio2">Incident Report</label>"+
-		"<input type="radio" name="radio" id="radio3" class="radio"/>"+
-		"<label for="radio3">Admin</label>"+
+		"<label for='radio1'>TSO</label>"+
+		"<input type='radio' name='radio' id='radio2' class='radio'/>"+
+		"<label for='radio2'>Incident Report</label>"+
+		"<input type='radio' name='radio' id='radio3' class='radio'/>"+
+		"<label for='radio3'>Admin</label>"+
 		"/div>"+
-		"<div class="inputForm">"+
-		"<form action = "" method="">"+
+		"<div class='inputForm'>"+
+		"<form action = '' method=''>"+
 		"User name/Reference Number:"+
-		"<input type="text" name="" >"+
+		"<input type='text' name='' >"+
 		"Password"+
-		"<input type="password" name="" >"+
-		"<input type="submit" value="LOGIN">"+
+		"<input type='password' name='' >"+
+		"<input type='submit' value='LOGIN'>"+
 		"</form>"+
 		"</div>"+
 		$(".body").html(body);
