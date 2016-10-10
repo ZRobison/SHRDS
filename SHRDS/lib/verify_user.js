b8f4e8c72e4b7f0d9970ab9e56e02056b0467f21
Tim Bishop <tim.bishop@students.mq.edu.au>
2016-10-11 10:59:27 +1100

    user verification stuff

--- SHRDS/lib/verify_user.js
+++ SHRDS/lib/verify_user.js
@@ -1,13 +1,21 @@
 function verify_user(user, password){
+    //If password is null, it is a IR user. ADD IN USERTYPE VARIABLE 
     if(password = null){
          MySql.Execute(
             "sql8.freemysqlhosting.net",
             "sql8110237", 
             "Gn1uMM2Adr", 
             "sql8110237", 
-            "select * from Users", 
+            "select "+user+"from Users", 
             function (data) {
-                document.getElementById("output").innerHTML = JSON.stringify(data,null,2);
+                //If we have an empty object, it means the user does not exist
+                if(data == null){
+                    window.alert("Error: That user number does not exist");
+                }
+                //Otherwise there is a match for the user number in the DB
+                else{
+                    location.href = 'index.html?#WHR';  
+                }
             });
         }
     else{
@@ -16,8 +24,16 @@ function verify_user(user, password){
             "sql8110237", 
             "Gn1uMM2Adr", 
             "sql8110237", 
-            "select * from Users", 
+            "select"+user+"from Users"
+            "where password ="+password+", 
             function (data) {
-                document.getElementById("output").innerHTML = JSON.stringify(data,null,2);
+                //If we have an empty object, it means the user does not exist
+                if(data == null){
+                    window.alert("Error: That user number does not exist");
+                }
+                //Otherwise there is a match for the user number in the DB
+                else{
+                    location.href = 'index.html?#WHR';  
+                }
                 }
             }
