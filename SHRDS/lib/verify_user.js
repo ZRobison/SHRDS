00ac8b8e3945560d097d1d140090007dcd4de6ac
MR Tim Bishop <41202392@students.mq.edu.au>
2016-10-12 17:14:42 +1100

    fixed verify user

--- SHRDS/lib/verify_user.js
+++ SHRDS/lib/verify_user.js
@@ -1,21 +1,23 @@
 var user = {
     verify_user: function () {
         //If password is null, it is a IR user. ADD IN USERTYPE VARIABLE 
+		//This is cirrently testing to a different database, will have to replace all db connection stuff later
         //if(password = null){
+			//dummy test db
             MySql.Execute(
-                "sql8.freemysqlhosting.net",
-                "sql8110237", 
-                "Gn1uMM2Adr", 
-                "sql8110237", 
-                "select "+document.getElementById("username")+" from Users Where password ="+document.getElementById("password"), 
-                function (data) {
-                    //If we have an empty object, it means the user does not exist
-                    if ( Object.keys(data).length<1){
-                        document.getElementById("passwordCheck").innerHTML = "Error: That username and password combination is incorrect";
+                "sql6.freemysqlhosting.net",
+                "sql6138853", 
+                "sHUtJmyDh3", 
+                "sql6138853", 
+                "select USER_ID from SHRDS_USER where USER_ID ="+document.getElementById("username").value, 					//+" and password ="+document.getElementById("password")  document.getElementById("username")
+                function (data) {		
+                    //If we have a match that means the user has the correct credentials
+                    if (parseInt(data.Result[0].USER_ID)===parseInt(document.getElementById("username").value)){
+							location.href = 'index.html?#WHR';  
                         }
                     //Otherwise there is a match for the user number in the DB
-                    else{
-                            location.href = 'index.html?#WHR';  
+                    else{           
+							document.getElementById("passwordCheck").innerHTML = "Error: That username and password combination is incorrect  "+document.getElementById("username").value+" "+ data.Result[0].USER_ID;
                         }
                     });
     } 
