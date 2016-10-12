82749f9634b7c49e21b3cf5e30a90531d1a4cfb6
ZeshengPeng <zesheng.peng@students.mq.edu.au>
2016-10-12 20:48:16 +1100

    edit html

--- SHRDS/js/views/IRandEventSpecificView.js
+++ SHRDS/js/views/IRandEventSpecificView.js
@@ -9,48 +9,48 @@ var body =
 
 "Arena:"+
 "<select>"+
-  "<option value="" selected>select one</option>"+
-  "<option value="">Arena 1</option>"+
-  "<option value="">Arena 2</option>"+
-  "<option value="">Arena 3</option>"+
-  "<option value="">Arena 4</option>"+
+  "<option value='' selected>select one</option>"+
+  "<option value=''>Arena 1</option>"+
+  "<option value=''>Arena 2</option>"+
+  "<option value=''>Arena 3</option>"+
+  "<option value=''>Arena 4</option>"+
   "</select>"+
 
  "   Age: "+
 "<select>"+
-  "<option value="" selected>Under 13</option>"+
-  "<option value="">Under 14</option>"+
-  "<option value="">Under 15</option>"+
-  "<option value="">Under 17</option>"+
-  "<option value="">Under 19</option>"+
-  "<option value="">open</option>"+
-  "<option value="">30-34</option>"+
-  "<option value="">35-39</option>"+
-  "<option value="">40-44</option>"+
-  "<option value="">45-49</option>"+
-  "<option value="">50-54</option>"+
-  "<option value="">55-59</option>"+
-  "<option value="">60-64</option>"+
-  "<option value="">65-69</option>"+
-  "<option value="">70+</option>"+
+  "<option value='' selected>Under 13</option>"+
+  "<option value=''>Under 14</option>"+
+  "<option value=''>Under 15</option>"+
+  "<option value=''>Under 17</option>"+
+  "<option value=''>Under 19</option>"+
+  "<option value=''>open</option>"+
+  "<option value=''>30-34</option>"+
+  "<option value=''>35-39</option>"+
+  "<option value=''>40-44</option>"+
+  "<option value=''>45-49</option>"+
+  "<option value=''>50-54</option>"+
+  "<option value=''>55-59</option>"+
+  "<option value=''>60-64</option>"+
+  "<option value=''>65-69</option>"+
+  "<option value=''>70+</option>"+
   "</select>"+
     "Heat:"+
 "<select>"+
-  "<option value="" selected>select one</option>"+
-  "<option value="">Heat 1</option>"+
-  "<option value="">Heat 2</option>"+
-  "<option value="">Heat 3</option>"+
-  "<option value="">Heat 4</option>"+
-  "<option value="">Heat 5</option>"+
+  "<option value='' selected>select one</option>"+
+  "<option value=''>Heat 1</option>"+
+  "<option value=''>Heat 2</option>"+
+  "<option value=''>Heat 3</option>"+
+  "<option value=''>Heat 4</option>"+
+  "<option value=''>Heat 5</option>"+
   "</select>"+
       "Round:"+
 "<select>"+
-  "<option value="" selected>select one</option>"+
-  "<option value="">Heat 1</option>"+
-  "<option value="">Heat 2</option>"+
-  "<option value="">Heat 3</option>"+
-  "<option value="">Heat 4</option>"+
-  "<option value="">Heat 5</option>"+
+  "<option value='' selected>select one</option>"+
+  "<option value=''>Heat 1</option>"+
+  "<option value=''>Heat 2</option>"+
+  "<option value=''>Heat 3</option>"+
+  "<option value=''>Heat 4</option>"+
+  "<option value=''>Heat 5</option>"+
   "</select>"+
   "<input type ='radio' name='' id=''/>"+
   "Quater Final"+
@@ -60,11 +60,11 @@ var body =
  " Grand Final"+
  "       Craft type:"+
 "<select>"+
-  "<option value="" selected>Select one</option>"+
-  "<option value="">Ski</option>"+
-  "<option value="">Double Ski</option>"+
-  "<option value="">Board</option>"+
-  "<option value="">Boat</option>"+
+  "<option value='' selected>Select one</option>"+
+  "<option value=''>Ski</option>"+
+  "<option value=''>Double Ski</option>"+
+  "<option value=''>Board</option>"+
+  "<option value=''>Boat</option>"+
   "</select>"+
   "Time:"+
   //put time here
