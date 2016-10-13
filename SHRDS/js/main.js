7de2e40ca79223cff195c8b8d54230efcaf943ea
donald rupin <donald rupin>
2016-10-13 14:11:55 +1100

    Improved behaviour of login view to react to what the user presses...

--- SHRDS/js/main.js
+++ SHRDS/js/main.js
@@ -58,6 +58,20 @@ var app = {
 		}else if (hash.match(app.prevailingURL)) {
 			new PrevailingView().render();
 		} 
+		
+		$("input[type='radio']").click(function()
+			{
+			var previousValue = $(this).attr('previousValue');
+			var name = $(this).attr('name');
+
+			if (previousValue == 'checked'){
+				$(this).removeAttr('checked');
+				$(this).attr('previousValue', false);
+			}else{
+				$("input[name="+name+"]:radio").attr('previousValue', false);
+				$(this).attr('previousValue', 'checked');
+			}
+		});
 	}
 };
 
