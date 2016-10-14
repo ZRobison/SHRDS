e65447e9e887c1094b26e950cb51637b58e5604f
ZeshengPeng <zesheng.peng@students.mq.edu.au>
2016-10-15 01:51:16 +1100

    seperate views html

---
+++ SHRDS/js/views/IRView.js
@@ -0,0 +1,55 @@
+function Incident Report(){
+var header ="Incident Report";
+
+var body =
+
+"<form action = '' method=''>"+
+"<button type='submit'>&#43;</button>"+ 
+"Fall Offs(by wave)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button> "+
+"Fall Offs(by collision)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button>"+ 
+"<button type='submit'>&#43;</button> "+
+"Back-shoot/Nose-Dive"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button> "+
+"Broaches"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button> "+
+"Flying Craft"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button>"+ 
+"Lost Craft(serious)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button> "+
+"Lost Craft(severe)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> "+
+"<button type='submit'>&#43;</button> "+
+"Collision(serious)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> <button type='submit'>&#43;</button>"+ 
+"Injury(minor)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> <button type='submit'>&#43;</button> "+
+"Injury(serious)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> <button type='submit'>&#43;</button> "+
+"Fall Offs(by wave)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> <button type='submit'>&#43;</button> "+
+"Injury(severe)"+
+"<input type='text' style='width:20px;' />"+
+"<button type='submit'>&#45;</button> ";
+var footer=
+"<button type='button' class='blueButtons'>back</button>"+
+"<button type='button' class='blueButtons'>Next</button>"+
+"<form>";
+}
