07cc7331df52e918367e7319e106420361ce597f
Tim Bishop <Tim Bishop>
2016-10-15 14:58:48 +1100

    comments

--- SHRDS/js/dftout.js
+++ SHRDS/js/dftout.js
@@ -1,14 +1,17 @@
-var login = function(){
-	//remember to make 0
-	var check = 1;
-	check = verify_user();
-	if (check){
-		location.href = 'index.html?#prevailing1';
-	} else {
-		// error message
-	}
-	
-	
-	
-}
+var login = function () {
+    //This function is not currently used
+    //All login logic is handled in verify_user, Ill switch it up it I can figure out some JS stuff later (tim)
 
+
+    //remember to make 0
+    var check = 1;
+    check = verify_user();
+    if (check) {
+        location.href = 'index.html?#prevailing1';
+    } else {
+        // error message
+    }
+
+
+
+}
