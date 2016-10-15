ea5db12f61ab7b2153864a2fb2ea2e6e296f0412
Tim Bishop <Tim Bishop>
2016-10-15 14:19:50 +1100

    fixed all linkages for different user/page

--- SHRDS/js/views/FormSelectView.js
+++ SHRDS/js/views/FormSelectView.js
@@ -1,7 +1,13 @@
-function formSelect(){
-var header ="Form Select"；
-var body =
-"<button type='submit'>Surf Hazard Rating Prevailing</button>"
-"<button type='submit'>Surf Hazard Rating Event Specific</button>"；
-	
+var FormSelectView = function () {
+    this.render = function () {
+        var header = "Form Select";
+        var body =
+            "<button type='submit'>Surf Hazard Rating Prevailing</button>" +
+            "<button type='submit'>Surf Hazard Rating Event Specific</button>";
+
+
+        $(".heading").text(header);
+        $(".body").html(body);
+        $(".footer").html(footer);
+    }
 }
--- SHRDS/js/views/PrevailingView.js
+++ SHRDS/js/views/PrevailingView.js
@@ -1,33 +1,33 @@
-var PrevailingView = function(){
+var PrevailingView = function () {
 
- 
-	this.render = function() {
 
-		
-		$(".heading").text("Prevailing Conditions");
+    this.render = function () {
 
-  var body=
-    "<div id='beachBox'><p>Beach<p>" +
-      "<select name='select' id='myList'>" +
-      "<option value='1'>Select One</option>" +
-      "<option value='2'>Beach1</option>" +
-      "<option value='3'>Beach2</option>" +
-      "<option value='4'>Beach3</option>" +
-      "<option value='5'>Beach4</option>" +
-    "</select>"+
-    "</div>"+
-      "<div id='windSpeedBox><p>Wind Speed</p>" +
-		"<p>We will fill this windspeed from BOM later</p>" +
-      "</div>"+
 
-		"<div id='nearestTideBox>"+
-            "<p>Nearest Tides</p>" +   
-            "<ul><li>Time1</li>" +    //I think there is the time of the 2 nearest tides??
-            "<li>Time2</li>" + 
+        $(".heading").text("Prevailing Conditions");
+
+        var body =
+            "<div id='beachBox'><p>Beach<p>" +
+            "<select name='select' id='myList'>" +
+            "<option value='1'>Select One</option>" +
+            "<option value='2'>Beach1</option>" +
+            "<option value='3'>Beach2</option>" +
+            "<option value='4'>Beach3</option>" +
+            "<option value='5'>Beach4</option>" +
+            "</select>" +
+            "</div>" +
+            "<div id='windSpeedBox><p>Wind Speed</p>" +
+            "<p>We will fill this windspeed from BOM later</p>" +
+            "</div>" +
+
+            "<div id='nearestTideBox>" +
+            "<p>Nearest Tides</p>" +
+            "<ul><li>Time1</li>" + //I think there is the time of the 2 nearest tides??
+            "<li>Time2</li>" +
             "<li>Height1</li>" +
-			"<li>Height2</li></ul>" +
-            "</div>"+
-      "<div id='windDirBox'><p>Wind Direction</p>" +
+            "<li>Height2</li></ul>" +
+            "</div>" +
+            "<div id='windDirBox'><p>Wind Direction</p>" +
             "<input type='radio' name='radio' id='radio1' class='radio'/>" +
             "<label for='radio1'>North</label>" +
             "<input type='radio' name='radio' id='radio2' class='radio'/>" +
@@ -45,21 +45,20 @@ var PrevailingView = function(){
             "<input type='radio' name='radio' id='radio8' class='radio'/>" +
             "<label for='radio8'>North West</label>" +
             "</div>";
-        
-            
-            //DONT KNOW WHAT THESE INPUT BOXES ARE FOR, MAY NOT NEED THEM WITH AUTFILL DATA FROM BOM"<input type='text' name='text3'/>" +
-            //"<input type='text' name='text3'/>" +
-		
-		$(".body").html(body);
-		
-		var footer = 
-			"<form action = 'index.html?#WHR' method='' onClick=''>" +
-			"<button type='submit' class='blueButtons'>Next</button>" +
-			"</form>";
-			
-			$(".footer").html(footer);
-			
-	}
+
+
+        //DONT KNOW WHAT THESE INPUT BOXES ARE FOR, MAY NOT NEED THEM WITH AUTFILL DATA FROM BOM"<input type='text' name='text3'/>" +
+        //"<input type='text' name='text3'/>" +
+
+        $(".body").html(body);
+
+        var footer =
+            "<form action = 'index.html?#WHR' method='' onClick=''>" +
+            "<button type='submit' class='blueButtons'>Next</button>" +
+            "</form>";
+
+        $(".footer").html(footer);
+
+    }
 
 }
-
