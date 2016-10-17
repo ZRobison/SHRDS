ab663c2d51f74fdb2b8a7858bad39a526e452974
Leon <digital_domain@hotmail.com>
2016-10-17 15:34:00 +1100

    Local update

--- SHRDS/js/views/FormSelectView.js
+++ SHRDS/js/views/FormSelectView.js
@@ -2,15 +2,14 @@ var FormSelectView = function () {
     this.render = function () {
         var header = "Form Select";
         var body =
-            "<div id='radBut'>" +
+            "<div class='formSelRad'>" +
             "<input type='radio' name='shrRadio' id='shrRadio1' class='radio' value='checked' >" +
             "<label for='shrRadio1'>Prevailing Surf Hazard Rating</label></div>" +
-            "<div id='radBut'>" +
+            "<div class='formSelRad'>" +
             "<input type='radio' name='shrRadio' id='shrRadio2' class='radio' value='checked' >" +
             "<label for='shrRadio2'>Event Specific Surf Hazard Rating</label></div>" ;      
 
-        var footer = "<button type='submit' class='blueButtons' id='submitBut' onclick='shrFormSelect()'>NEXT</button>";
-
+        var footer = "<button type='submit' class='blueButtons' onclick='shrFormSelect()'>Next</button>";
 
         $(".heading").text(header);
         $(".body").html(body);
