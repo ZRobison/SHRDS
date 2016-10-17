d1ad98689d850aef11583962a2435a7c4a4577f7
Leon <digital_domain@hotmail.com>
2016-10-17 13:55:42 +1100

    update

---
+++ SHRDS/js/change_password.js
@@ -0,0 +1,39 @@
+function change_password() {
+    if (document.getElementById("userID").value == "" || document.getElementById("userPassword").value == "") {
+        $(".message").text("Error: Please enter a username and new password");
+        return;
+    }
+    //This is a gross way of updating passwords. 
+    //TO DO: An update statement does not return anything using this mysql.js library. Maybe imbed a select in the upsate to force a data return
+    MySql.Execute(
+        dbconfig.host,
+        dbconfig.dbUser,
+        dbconfig.dbPassword,
+        dbconfig.dbUser,
+        "SELECT USER_ID FROM SHRDS_USER WHERE USER_ID ='" + document.getElementById("userID").value + "'",
+        function (data) {
+            if (data.Success === true) {
+                console.log(JSON.stringify(data));
+                //If that user exists, continue with the password change
+                if (data.Result[0] != null && data.Result[0] != "") {
+                    if (data.result.USER_ID == document.getElementById("userID").value) {
+                        MySql.Execute(
+                            dbconfig.host,
+                            dbconfig.dbUser,
+                            dbconfig.dbPassword,
+                            dbconfig.dbUser,
+                            "update SHRDS_USER set PASSWORD ='" + document.getElementById("userPassword").value + "' where USER_ID ='" + document.getElementById("userID").value + "'",
+                            function (data) {}
+
+                        );
+                    }
+                    $(".message").text("Success: You have changed the password of user: " + document.getElementById("userID").value);
+                } else {
+                    $(".message").text("That is not a valid user ID");
+                }
+            }
+        }
+    );
+
+
+}
