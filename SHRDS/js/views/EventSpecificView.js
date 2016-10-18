830924bc7ede3978100b079cd1daa6cc944e0116
Leon <digital_domain@hotmail.com>
2016-10-18 17:13:47 +1100

    LM update

--- SHRDS/js/views/EventSpecificView.js
+++ SHRDS/js/views/EventSpecificView.js
@@ -1,20 +1,21 @@
 var EventSpecificView = function () {
 
     this.render = function () {
-		var header = "";
-		if (app.SHRFlag == 2){
-			header = "Event specific SHR";
-		} else {
-			header = "Event specific Incident Report";
-		}
-        
+        var header = "";
+        if (app.SHRFlag == 2) {
+            header = "Event specific SHR";
+        } else {
+            header = "Event specific Incident Report";
+        }
+
 
 
         var body =
 
             "Arena:" +
-            "<select>" +
-            "<option value='' selected>select one</option>" +
+            "<div class='eventSpecificBlock'>" +
+            "<select class='eventSpecificSelects'>" +
+            "<option value='' selected>Select one</option>" +
             "<option value=''>Arena 1</option>" +
             "<option value=''>Arena 2</option>" +
             "<option value=''>Arena 3</option>" +
@@ -22,7 +23,7 @@ var EventSpecificView = function () {
             "</select>" +
 
             "   Age: " +
-            "<select>" +
+            "<select class='eventSpecificSelects'>" +
             "<option value='' selected>Under 13</option>" +
             "<option value=''>Under 14</option>" +
             "<option value=''>Under 15</option>" +
@@ -40,8 +41,8 @@ var EventSpecificView = function () {
             "<option value=''>70+</option>" +
             "</select>" +
             "Heat:" +
-            "<select>" +
-            "<option value='' selected>select one</option>" +
+            "<select class='eventSpecificSelects'>" +
+            "<option value='' selected>Select one</option>" +
             "<option value=''>Heat 1</option>" +
             "<option value=''>Heat 2</option>" +
             "<option value=''>Heat 3</option>" +
@@ -49,22 +50,32 @@ var EventSpecificView = function () {
             "<option value=''>Heat 5</option>" +
             "</select>" +
             "Round:" +
-            "<select>" +
-            "<option value='' selected>select one</option>" +
+            "<select class='eventSpecificSelects'>" +
+            "<option value='' selected>Select one</option>" +
             "<option value=''>Heat 1</option>" +
             "<option value=''>Heat 2</option>" +
             "<option value=''>Heat 3</option>" +
             "<option value=''>Heat 4</option>" +
             "<option value=''>Heat 5</option>" +
             "</select>" +
+
+            "<div class='eventRadios'>" +
             "<input type ='radio' name='' id=''/>" +
             "Quater Final" +
+            "</div>" +
+
+            "<div class='eventRadios'>" +
             " <input type ='radio' name='' id=''/>" +
             "Semi Final" +
+            "</div>" +
+
+            "<div class='eventRadios'>" +
             " <input type ='radio' name='' id=''/>" +
             " Grand Final" +
-            "       Craft type:" +
-            "<select>" +
+            " Craft type:" +
+            "</div>" +
+
+            "<select class='eventSpecificSelects'>" +
             "<option value='' selected>Select one</option>" +
             "<option value=''>Ski</option>" +
             "<option value=''>Double Ski</option>" +
@@ -74,18 +85,23 @@ var EventSpecificView = function () {
             "Time:" +
             //put time here
 
-            "in/out:" +
+            "<div class='eventRadios'>" +
+            "In/Out:" +
             "<input type ='radio' name='inOut' id='in'/>" +
             "Out" +
             "<input type ='radio' name='' id='out'/>" +
             "In" +
+            "</div>" +
+
+
             "beach/location:<input type = 'text' name='bl'>";
 
         var footer =
             //THIS IS A BROKEN WAY OF DOING THIS, IF AN IR USES THIS FORM THEY START FILLING OUT AN SHR. 
             "<form action= '' >" +
             "<button type='submit' class='blueButtons' onclick='routeIS()'>Next</button> " +
-            "</form>";
+            "</form>" +
+            "</div>";
 
         $(".heading").text(header);
         $(".body").html(body);
@@ -94,10 +110,10 @@ var EventSpecificView = function () {
 }
 
 
-function routeIS(){
-	if (app.SHRFlag == 0){
-		location.href = 'index.html?#incident1';
-	} else {
-		location.href = 'index.html?#WHR';
-	}
+function routeIS() {
+    if (app.SHRFlag == 0) {
+        location.href = 'index.html?#incident1';
+    } else {
+        location.href = 'index.html?#WHR';
+    }
 }
