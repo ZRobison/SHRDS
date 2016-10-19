f220d2b569290c3c09464e046315ec591d4fde57
Al Barnes <Al Barnes>
2016-10-20 00:03:44 +1100

    updated data object

--- SHRDS/js/views/EventSpecificView.js
+++ SHRDS/js/views/EventSpecificView.js
@@ -11,7 +11,8 @@ var EventSpecificView = function () {
 
 
         var body =
-
+            "<ul>" +
+			"<li>" +
             "Arena:" +
             "<select>" +
             "<option value='' selected>select one</option>" +
@@ -20,7 +21,8 @@ var EventSpecificView = function () {
             "<option value=''>Arena 3</option>" +
             "<option value=''>Arena 4</option>" +
             "</select>" +
-
+            "</li>" +
+			"<li>" +
             "   Age: " +
             "<select>" +
             "<option value='' selected>Under 13</option>" +
@@ -39,6 +41,8 @@ var EventSpecificView = function () {
             "<option value=''>65-69</option>" +
             "<option value=''>70+</option>" +
             "</select>" +
+			"</li>" +
+			"<li>" +
             "Heat:" +
             "<select>" +
             "<option value='' selected>select one</option>" +
@@ -48,6 +52,8 @@ var EventSpecificView = function () {
             "<option value=''>Heat 4</option>" +
             "<option value=''>Heat 5</option>" +
             "</select>" +
+			"</li>" +
+			"<li>" +
             "Round:" +
             "<select>" +
             "<option value='' selected>select one</option>" +
@@ -57,13 +63,17 @@ var EventSpecificView = function () {
             "<option value=''>Heat 4</option>" +
             "<option value=''>Heat 5</option>" +
             "</select>" +
+			"</li>" +
+			"<li>" +
             "<input type ='radio' name='' id=''/>" +
             "Quater Final" +
             " <input type ='radio' name='' id=''/>" +
             "Semi Final" +
             " <input type ='radio' name='' id=''/>" +
             " Grand Final" +
-            "       Craft type:" +
+			"</li>" +
+			"<li>" +
+            "Craft type:" +
             "<select>" +
             "<option value='' selected>Select one</option>" +
             "<option value=''>Ski</option>" +
@@ -71,15 +81,24 @@ var EventSpecificView = function () {
             "<option value=''>Board</option>" +
             "<option value=''>Boat</option>" +
             "</select>" +
+			"</li>" +
+			"<li>" +
             "Time:" +
             //put time here
-
+            "</li>" +
+			"<li>" +
             "in/out:" +
             "<input type ='radio' name='inOut' id='in'/>" +
+			"</li>" +
+			"<li>" +
             "Out" +
             "<input type ='radio' name='' id='out'/>" +
+			"</li>" +
+			"<li>" +
             "In" +
-            "beach/location:<input type = 'text' name='bl'>";
+            "beach/location:<input type = 'text' name='bl'>" +
+			"</li>" +
+			"</ul>";
 
         var footer =
             //THIS IS A BROKEN WAY OF DOING THIS, IF AN IR USES THIS FORM THEY START FILLING OUT AN SHR. 
