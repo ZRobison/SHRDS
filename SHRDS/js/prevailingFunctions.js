927b57b6e316ca434e458585c25174a38f551fd2
MR Daniel Barnes <41749952@students.mq.edu.au>
2016-10-21 14:06:21 +1100

    framework for updates

---
+++ SHRDS/js/prevailingFunctions.js
@@ -0,0 +1,9 @@
+// prevailing functions
+
+function getPrevailingBeach () {
+	//app.prevalingSHRData.SHR.pWHR = $("select[name=WHRForm]").val();
+      //  score = app.prevalingSHRData.SHR.pWHR
+	  app.prevalingHomePageData.beach = $("select[name=WHRForm]").val();
+       score = app.prevalingHomePageData.beach
+	
+}
