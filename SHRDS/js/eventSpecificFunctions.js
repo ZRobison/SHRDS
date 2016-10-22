bb67bc25e9682c86d2aead0492efd50858c1c4cf
Al Barnes <Al Barnes>
2016-10-22 19:57:54 +1100

    Completed event specific front page (see developer tools console log) - As per Zac's error checking methods

--- SHRDS/js/eventSpecificFunctions.js
+++ SHRDS/js/eventSpecificFunctions.js
@@ -1,46 +1,111 @@
 // event specific functions 
+function getESValues(){
+	var check = true;
+	check = check && arenaES();
+	check = check && ageES();
+	check = check && heatES();
+	check = check && roundES();
+	check = check && finalES();
+	check = check && craftTypeES();
+	check = check && inOutES();
+	check = check && beachLocationES();
+	if (check) {
+		 app.esSHRData.time = getDateTime();
+		window.location.hash = "#WHR";
+		console.log(app.esSHRData.time);
+	} else {
+		console.log("error");
+		$(".error").text("Please fill in all fields.");
+	}
+}
+
 
 function arenaES() {
-	  app.esSHRData.arena = $("select[name=ESArena]").val();
-      console.log(app.esSHRData.arena);
+	  var score = $("select[name=ESArena]").val();
+	  if (score == '-1') {
+		  return false;
+} else {
+		app.esSHRData.arena = score;
+		console.log(app.esSHRData.arena);
+		  return true;
+	  }
 }
 
 function ageES() {
-	  app.esSHRData.age = $("select[name=ESAge]").val();
-      console.log(app.esSHRData.age);
+	var score = $("select[name=ESAge]").val();
+	if (score == '-1') {
+		  return false;
+} else {
+	  app.esSHRData.age = score; 
+	  console.log(app.esSHRData.age);
+      return true;
+	  }
 }
 
+
 function heatES() {
-	  app.esSHRData.heat = $("select[name=ESHeat]").val();
-      console.log(app.esSHRData.heat);
+	var score = $("select[name=ESHeat]").val();
+	if (score == '-1') {
+		  return false;
+} else {
+	  app.esSHRData.heat = score; 
+	  console.log(app.esSHRData.heat);
+      return true;
+	  }
 }
 
 function roundES() {
-	  app.esSHRData.round = $("select[name=ESRound]").val();
-      console.log(app.esSHRData.round);
+	var score = $("select[name=ESRound]").val();
+	if (score == '-1') {
+		  return false;
+} else {
+	  app.esSHRData.round = score; 
+	  console.log(app.esSHRData.round);
+      return true;
+	  }
 }
 
 function finalES() {
-	  app.esSHRData.finalType = $("select[name=ESFinalType]").val();
-      console.log(app.esSHRData.finalType);
+	var score = $("select[name=ESFinalType]").val();
+	if (score == '-1') {
+		  return false;
+} else {
+	  app.esSHRData.finalType = score; 
+	  console.log(app.esSHRData.finalType);
+      return true;
+	  }
 }
 
 function craftTypeES() {
-	  app.esSHRData.craftType = $("select[name=ESCraftType]").val();
-      console.log(app.esSHRData.craftType);
+	var score = $("select[name=ESCraftType]").val();
+	if (score == '-1') {
+		  return false;
+} else {
+	  app.esSHRData.craftType = score; 
+	  console.log(app.esSHRData.craftType);
+      return true;
+	  }
 }
 
 function inOutES() {
-	  app.esSHRData.inOrOut = $("input[name=ESInOut]").val();
-      console.log(app.esSHRData.inOrOut);
+	var score = $("input[name=ESInOut]").val();
+	if (score == "") {
+		  return false;
+} else {
+	  app.esSHRData.inOrOut = score; 
+	  console.log(app.esSHRData.inOrOut);
+      return true;
+	  }
 }
+
 
 function beachLocationES() {
-	  app.esSHRData.beach = $("input[name=ESBeach]").val();
-      console.log(app.esSHRData.beach);
-}
-
-function timeES() {
-	  //app.esSHRData.time = $("getDateTime()").val();
-     // console.log(app.esSHRData.time);
+	var score = $("input[name=ESBeach]").val();
+	if (score == "") {
+		  return false;
+} else {
+	  app.esSHRData.beach = score; 
+	  console.log(app.esSHRData.beach);
+      return true;
+	  }
 }
