5f1d5a4067ff649f1bc24bb778bb0e3624217398
Tim Bishop <Tim Bishop>
2016-10-23 14:30:44 +1100

    changes for data storage

--- SHRDS/js/views/EventSpecificView.js
+++ SHRDS/js/views/EventSpecificView.js
@@ -19,86 +19,87 @@ var EventSpecificView = function () {
             "<option value=''>Arena 2</option>" +
             "<option value=''>Arena 3</option>" +
             "<option value=''>Arena 4</option>" +
-			"<option value=''>Arena 5</option>" +
-			"<option value=''>Arena 6</option>" +
-			"<option value=''>Arena 7</option>" +
-			"<option value=''>Arena 8</option>" +
-		    "<option value=''>Arena 9</option>" +
-		    "<option value=''>Arena 10</option>" +
+            "<option value=''>Arena 5</option>" +
+            "<option value=''>Arena 6</option>" +
+            "<option value=''>Arena 7</option>" +
+            "<option value=''>Arena 8</option>" +
+            "<option value=''>Arena 9</option>" +
+            "<option value=''>Arena 10</option>" +
             "</select>" +
 
             "<div class='irTitles'>Age:</div>" +
             "<select name='ESAge'>" +
-            "<option value='-1' selected>Under 13</option>" +
-            "<option value=''>Under 14</option>" +
-            "<option value=''>Under 15</option>" +
-            "<option value=''>Under 17</option>" +
-            "<option value=''>Under 19</option>" +
-            "<option value=''>open</option>" +
-            "<option value=''>30-34</option>" +
-            "<option value=''>35-39</option>" +
-            "<option value=''>40-44</option>" +
-            "<option value=''>45-49</option>" +
-            "<option value=''>50-54</option>" +
-            "<option value=''>55-59</option>" +
-            "<option value=''>60-64</option>" +
-            "<option value=''>65-69</option>" +
-            "<option value=''>70+</option>" +
+            "<option value='-1' selected>Select one</option>" +
+            "<option value='Under 13'>Under 13</option>" +
+            "<option value='Under 14'>Under 14</option>" +
+            "<option value='Under 15'>Under 15</option>" +
+            "<option value='Under 17'>Under 17</option>" +
+            "<option value='Under 19'>Under 19</option>" +
+            "<option value='30-34'>30-34</option>" +
+            "<option value='35-39'>35-39</option>" +
+            "<option value='40-44'>40-44</option>" +
+            "<option value='45-49'>45-49</option>" +
+            "<option value='50-54'>50-54</option>" +
+            "<option value='55-59'>55-59</option>" +
+            "<option value='60-64'>60-64</option>" +
+            "<option value='65-69'>65-69</option>" +
+            "<option value='70+'>70+</option>" +
+            "<option value='open'>Open</option>" +
             "</select>" +
 
             "<div class='irTitles'>Heat:</div>" +
             "<select name='ESHeat'>" +
             "<option value='-1' selected>Select one</option>" +
-            "<option value=''>Heat 1</option>" +
-            "<option value=''>Heat 2</option>" +
-            "<option value=''>Heat 3</option>" +
-            "<option value=''>Heat 4</option>" +
-            "<option value=''>Heat 5</option>" +
+            "<option value='1'>Heat 1</option>" +
+            "<option value='2'>Heat 2</option>" +
+            "<option value='3'>Heat 3</option>" +
+            "<option value='4'>Heat 4</option>" +
+            "<option value='5'>Heat 5</option>" +
             "</select>" +
 
             "<div class='irTitles'>Round:</div>" +
             "<select name='ESRound'>" +
             "<option value='-1' selected>Select one</option>" +
-            "<option value=''>Round 1</option>" +
-            "<option value=''>Round 2</option>" +
-            "<option value=''>Round 3</option>" +
-            "<option value=''>Round 4</option>" +
-            "<option value=''>Round 5</option>" +
+            "<option value='1'>Round 1</option>" +
+            "<option value='2'>Round 2</option>" +
+            "<option value='3'>Round 3</option>" +
+            "<option value='4'>Round 4</option>" +
+            "<option value='5'>Round 5</option>" +
             "</select>" +
 
             "<div class='irTitles'>Final:</div>" +
-			"<select name='ESFinalType'>" +
+            "<select name='ESFinalType'>" +
             "<option value='-1'selected>Select one</option>" +
-            "<option value=''>Quater Final</option>" +
-            "<option value=''>Semi Final</option>" +
-            "<option value=''>Grand Final</option>" +
+            "<option value='Qualifier'>Qualifier</option>" +
+            "<option value='Quater Final'>Quater Final</option>" +
+            "<option value='Semi Final'>Semi Final</option>" +
+            "<option value='Grand Final'>Grand Final</option>" +
             "</select>" +
-            
+
             "<div class='irTitles'>Craft Type:</div>" +
             "<select name='ESCraftType'>" +
             "<option value='-1' selected>Select one</option>" +
-            "<option value=''>Ski</option>" +
-            "<option value=''>Double Ski</option>" +
-            "<option value=''>Board</option>" +
-            "<option value=''>Boat</option>" +
+            "<option value='Ski'>Ski</option>" +
+            "<option value='Double Ski'>Double Ski</option>" +
+            "<option value='Board'>Board</option>" +
+            "<option value='Boat'>Boat</option>" +
             "</select>" +
-			
-			"<div class='irTitles'>In/Out:</div>" +
+
+            "<div class='irTitles'>In/Out:</div>" +
             "<div id='inlineOutIn'>" +
-            "<input type ='radio' name='ESInOut' id='in'/>" +
+            "<input type ='radio' name='ESInOut' id='in' value='in'/>" +
             "Out" +
-            "<input type ='radio' name='ESInOut' id='out'/>" +
+            "<input type ='radio' name='ESInOut' id='out' value='out'/>" +
             "In" +
             "<div>" +
             "<br>Beach/location:<input type = 'text' name='ESBeach'>" +
             "</div>";
-			
 
-           
+
+
 
 
         var footer =
-            //THIS IS A BROKEN WAY OF DOING THIS, IF AN IR USES THIS FORM THEY START FILLING OUT AN SHR. Is this an old issue? 22/10/2016 
             "<form action= '' >" +
             "<button type='submit' class='blueButtons' onclick='getESValues()'>Next</button> " +
             "<button type='button' class='exitButton'>EXIT</button>" +
@@ -119,5 +120,3 @@ function routeIS() {
         location.href = 'index.html?#WHR';
     }
 }
-
-
