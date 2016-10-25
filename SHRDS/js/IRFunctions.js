4e5c9e63ecd86b64048017c599d569e237f95909
MR Daniel Barnes <41749952@students.mq.edu.au>
2016-10-25 19:22:14 +1100

    working IR with IN/OUT of the same page. Both IN & OUT values saving to the data object

--- SHRDS/js/IRFunctions.js
+++ SHRDS/js/IRFunctions.js
@@ -177,3 +177,184 @@ function getIRValues() {
 function irComplete() {
     window.location.hash = "#irComplete";
 }
+////////
+////////
+////////
+
+//IR 2 - OUT Functions  TRANSFER TO ORIGINAL 
+var numMin2 = 0;
+var numMax2 = 20;
+var gPMResultFOWave2 = 0;
+var gPMResultFOColl2 = 0;
+var gPMResultBSND2 = 0;
+var gPMResultBroach2 = 0;
+var gPMResultFlyingCraft2 = 0;
+var gPMResultLCSerious2 = 0;
+var gPMResultLCSevere2 = 0;
+var gPMResultCollisionSer2 = 0;
+var gPMResultINJMin2 = 0;
+var gPMResultINJSer2 = 0;
+var gPMResultINJSev2 = 0;
+var gPMResultDNF2 = 0;
+
+function addOUT(param) {
+    var numLoggedA2 = 1;
+    gPMResult2 = 1;
+    var num2 = document.getElementById("numOUT" + param).innerHTML;
+    if (num2 == numMax2 || num2 > numMax2) {
+        num2 = numMax2;
+        numLoggedA2 = 20;
+        gPMResult2 = numLoggedA2;
+    } else {
+        document.getElementById("numOUT" + param).innerHTML++;
+        numLoggedA2 = (((numLoggedA2) * 1) + ((num2) * 1));
+        gPMResult2 = numLoggedA2;
+    }
+    console.log(numLoggedA2);
+    console.log(gPMResult2);
+    return gPMResult2;
+}
+
+function minusOUT(param) {
+    var numLoggedM2 = -1;
+    gPMResult2 = -1;
+    var num2 = document.getElementById("numOUT" + param).innerHTML;
+    if (num2 == numMin2 || num2 < numMin2) {
+        num2 = numMin2;
+        numLoggedM2 = 0;
+        gPMResult2 = numLoggedM2;
+    } else {
+        document.getElementById("numOUT" + param).innerHTML--;
+        numLoggedM2 = (((numLoggedM2) * 1) + ((num2) * 1));
+        gPMResult2 = numLoggedM2;
+    }
+    console.log(numLoggedM2);
+    console.log(gPMResult2);
+    return gPMResult2;
+}
+// data storing functions
+// placing values to variables
+function incidentsReportedOneOUT() {
+    if ('addOUT(1)' || 'minusOUT(1)') {
+        gPMResultFOWave2 = gPMResult2;
+        return gPMResultFOWave2;
+    }
+}
+
+function incidentsReportedTwoOUT() {
+    if ('addOUT(2)' || 'minusOUT(2)') {
+        gPMResultFOColl2 = gPMResult2;
+        return gPMResultFOColl2;
+    }
+}
+
+function incidentsReportedThreeOUT() {
+    if ('addOUT(3)' || 'minusOUT(3)') {
+        gPMResultBSND2 = gPMResult2;
+        return gPMResultBSND2;
+    }
+}
+
+function incidentsReportedFourOUT() {
+    if ('addOUT(4)' || 'minusOUT(4)') {
+        gPMResultBroach2 = gPMResult2;
+        return gPMResultBroach2;
+    }
+}
+
+function incidentsReportedFiveOUT() {
+    if ('addOUT(5)' || 'minusOUT(5)') {
+        gPMResultFlyingCraft2 = gPMResult2;
+        return gPMResultFlyingCraft2;
+    }
+}
+
+function incidentsReportedSixOUT() {
+    if ('addOUT(6)' || 'minusOUT(6)') {
+        gPMResultLCSerious2 = gPMResult2;
+        return gPMResultLCSerious2;
+    }
+}
+
+function incidentsReportedSevenOUT() {
+    if ('addOUT(7)' || 'minusOUT(7)') {
+        gPMResultLCSevere2 = gPMResult2;
+        return gPMResultLCSevere2;
+    }
+}
+
+function incidentsReportedEightOUT() {
+    if ('addOUT(8)' || 'minusOUT(8)') {
+        gPMResultCollisionSer2 = gPMResult2;
+        return gPMResultCollisionSer2;
+    }
+}
+
+function incidentsReportedNineOUT() {
+    if ('addOUT(9)' || 'minusOUT(9)') {
+        gPMResultINJMin2 = gPMResult2;
+        return gPMResultINJMin2;
+    }
+}
+
+function incidentsReportedTenOUT() {
+    if ('addOUT(10)' || 'minusOUT(10)') {
+        gPMResultINJSer2 = gPMResult2;
+        return gPMResultINJSer2;
+    }
+}
+
+function incidentsReportedElevenOUT() {
+    if ('addOUT(11)' || 'minusOUT(11)') {
+        gPMResultINJSev2 = gPMResult2;
+        return gPMResultINJSev2;
+    }
+}
+
+function incidentsReportedTwelveOUT() {
+    if ('addOUT(12)' || 'minusOUT(12)') {
+        gPMResultDNF2 = gPMResult2;
+        return gPMResultDNF2;
+    }
+}
+// function for incident values 
+function getIRValuesOUT() {
+    // storing variables that hold the value to memory 
+    app.esIRData.IROUT.FOWave = gPMResultFOWave2;
+    console.log(app.esIRData.IROUT.FOWave);
+    //
+    app.esIRData.IROUT.pFOCollision = gPMResultFOColl2;
+    console.log(app.esIRData.IROUT.pFOCollision);
+    //
+    app.esIRData.IROUT.pBSND = gPMResultBSND2;
+    console.log(app.esIRData.IROUT.pBSND);
+    //
+    app.esIRData.IROUT.pBroach = gPMResultBroach2;
+    console.log(app.esIRData.IROUT.pBroach);
+    //
+    app.esIRData.IROUT.pFlyingCraft = gPMResultFlyingCraft2;
+    console.log(app.esIRData.IROUT.pFlyingCraft);
+    //
+    app.esIRData.IROUT.pLCSerious = gPMResultLCSerious2;
+    console.log(app.esIRData.IROUT.pLCSerious);
+    //
+    app.esIRData.IROUT.pLCSevere = gPMResultLCSevere2;
+    console.log(app.esIRData.IROUT.pLCSevere);
+    //
+    app.esIRData.IROUT.pCollisionSer = gPMResultCollisionSer2;
+    console.log(app.esIRData.IROUT.pCollisionSer);
+    //
+    app.esIRData.IROUT.pINJMin = gPMResultINJMin2;
+    console.log(app.esIRData.IROUT.pINJMin);
+    //
+    app.esIRData.IROUT.pINJSer = gPMResultINJSer2;
+    console.log(app.esIRData.IROUT.pINJSer);
+    //
+    app.esIRData.IROUT.pINJSev = gPMResultINJSev2;
+    console.log(app.esIRData.IROUT.pINJSev);
+    //
+    app.esIRData.IROUT.pDNF = gPMResultDNF2;
+    console.log(app.esIRData.IROUT.pDNF);
+    //
+}
+
