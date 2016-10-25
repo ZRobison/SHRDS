c4d6992d699573051b6fe9599e070b0178634535
Tim Bishop <tim.bishop@students.mq.edu.au>
2016-10-25 11:30:47 +1100

    fixing/breaking everything

--- SHRDS/js/adminFunctions.js
+++ SHRDS/js/adminFunctions.js
@@ -7,13 +7,17 @@ function adminPageSelect() {
 }
 
 //
+
 function getFormMetaData() {
+    var data1;
+    var data2;
     var name = "<br>";
     var formType = "<br>"
     var raceDeets = "<br>";
     var raceTime = "<br>";
+    var notify = -1;
     //Get ir form metadata
-    sql = "SELECT FIRST_NAME, LAST_NAME, SHR.RACE_ID, AGE_GROUP, CRAFT_TYPE, SHR.TIME " +
+    var sql = "SELECT FIRST_NAME, LAST_NAME, SHR.RACE_ID, AGE_GROUP, CRAFT_TYPE, SHR.TIME " +
         "FROM SHR, RACE, SHRDS_USER " +
         "WHERE SHR.RACE_ID = RACE.RACE_ID AND SHR.USER_ID = SHRDS_USER.USER_ID";
     console.log("about to fire form metadata sql");
@@ -22,10 +26,35 @@ function getFormMetaData() {
         dbconfig.dbUser,
         dbconfig.dbPassword,
         dbconfig.dbUser,
-        "SELECT FIRST_NAME, LAST_NAME, SHR.RACE_ID, AGE_GROUP, CRAFT_TYPE, SHR.TIME " +
-        "FROM SHR, RACE, SHRDS_USER " +
-        "WHERE SHR.RACE_ID = RACE.RACE_ID AND SHR.USER_ID = SHRDS_USER.USER_ID",
+        sql,
         function (data) {
+            //Get Prevailing form metadata
+            sql = "SELECT FIRST_NAME, LAST_NAME, TIME " +
+                "FROM SHR, SHRDS_USER " +
+                "WHERE SHR.USER_ID = SHRDS_USER.USER_ID";
+            console.log("about to fire callback2");
+            MySql.Execute(
+                dbconfig.host,
+                dbconfig.dbUser,
+                dbconfig.dbPassword,
+                dbconfig.dbUser,
+                sql,
+                function (data) {
+                    if (data.Result != null && data.Result != "") {
+                        for (var i = 0; i < data.Result.length; i++) {
+                            name += data.Result[i].FIRST_NAME + " " + data.Result[i].LAST_NAME + "<br>";
+                            formType += "Prevailing SHR" + "<br>";
+                            raceDeets += "<br>";
+                            raceTime += data.Result[i].TIME + "<br>";
+                        }
+                        document.getElementById("supervisingTSO").innerHTML += name;
+                        document.getElementById("raceDetails").innerHTML += raceDeets;
+                        document.getElementById("submittedTime").innerHTML += raceTime;
+                        document.getElementById("formType").innerHTML += formType;
+
+                    }
+                }
+            );
             console.log(JSON.stringify(data));
             if (data.Result != null && data.Result != "") {
                 for (var i = 0; i < data.Result.length; i++) {
@@ -34,12 +63,10 @@ function getFormMetaData() {
                     raceDeets += data.Result[i].AGE_GROUP + " " + data.Result[i].CRAFT_TYPE + "<br>";
                     raceTime += data.Result[i].TIME + "<br>";
                 }
-                document.getElementById("supervisingTSO").innerHTML += name;
-                document.getElementById("raceDetails").innerHTML += raceDeets;
-                document.getElementById("submittedTime").innerHTML += raceTime;
-                document.getElementById("formType").innerHTML += formType;
+
 
             }
+
         }
     );
 
