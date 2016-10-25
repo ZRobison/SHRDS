f2fa04e65b7affb014fd75adda5c78aaa63005ff
Leon <digital_domain@hotmail.com>
2016-10-25 20:50:04 +1100

    before

--- SHRDS/js/views/PrevailingView.js
+++ SHRDS/js/views/PrevailingView.js
@@ -7,9 +7,12 @@ var PrevailingView = function () {
         $(".heading").text("Prevailing Conditions");
 
         var body =
-            
-     
-            "<div id='beachBox'><p>Beach<p>" +
+
+            "<div id='prevailingWrapper'> " +
+
+            "<div id='windBox'>" +
+
+            "<div class='textPrev'>Beach</div>" +
             "<select name='prevailingBeach'>" +
             "<option value='-1'>Select One</option>" +
             "<option value='Beach1'>Beach1</option>" +
@@ -17,40 +20,42 @@ var PrevailingView = function () {
             "<option value='Beach3'>Beach3</option>" +
             "<option value='Beach4'>Beach4</option>" +
             "</select>" +
-            "</div>" +
 
-			"<div id='windDirBox'><p>Wind Direction<p>" +
+            "<div class='textPrev'>Wind Direction</div>" +
             "<select name='windDirection'>" +
             "<option value='-1'>Select One</option>" +
             "<option value='North'>North</option>" +
             "<option value='North East'>North East</option>" +
             "<option value='East'>East</option>" +
             "<option value='South East'>South East</option>" +
-			"<option value='South'>South</option>" +
-			"<option value='South West'>South West</option>" +
-			"<option value='West'>West</option>" +
-			"<option value='North West'>North West</option>" +
+            "<option value='South'>South</option>" +
+            "<option value='South West'>South West</option>" +
+            "<option value='West'>West</option>" +
+            "<option value='North West'>North West</option>" +
             "</select>" +
-            "</div>" +
-			
-            "<div id='windDirBox'><p>Wind Speed</p>" +
-			"<input type='text' name='windSpeed' placeholder='10 Knotts'>"+
-            "</div>" +
 
-            
-            "<div id='nearestTideBox'>" +
-			"<p>Nearest Tides</p>" +
-            "<ul id='tidesListBox'><li>Low tide time</li>" +
-			"<li><input type='text' name='tideTimeLow' placeholder='hr:mi:se'></li>"+			//I think there is the time of the 2 nearest tides??
-            "<li>High tide time</li>" +
-			"<li><input type='text' name='tideTimeHigh' placeholder='hr:mi:se'></li>"+
-            "<li>Low tide Height</li>" +
-			"<li><input type='text' name='tideHieghtLow' placeholder='Height'></li>"+
-            "<li>High tide Height</li>" +
-			"<li><input type='text' name='tideHieghtHigh' placeholder='Height'></li></ul>"+
-            "</div>"+
-			
-			"<div class='error'></div>";
+            "<div class='textPrev'>Wind Speed</div><br><br>" +
+            "<input type='text' name='windSpeed' placeholder='10 Knotts'>" +
+
+            "</div>" + //wind
+
+            "<div id='tideBox'>" +
+
+            "<div class='textPrev'>Nearest Tides</div>" +
+            "<div class='textPrev'>Low tide time</div>" +
+            "<input type='text' name='tideTimeLow' placeholder='hr:mi:se'>" + //I think there is the time of the 2 nearest tides??
+            "<div class='textPrev'>High tide time</div>" +
+            "<input type='text' name='tideTimeHigh' placeholder='hr:mi:se'>" +
+            "<div class='textPrev'>Low tide Height</div>" +
+            "<input type='text' name='tideHieghtLow' placeholder='Height'>" +
+            "<div class='textPrev'>High tide Height</div>" +
+            "<input type='text' name='tideHieghtHigh' placeholder='Height'>" +
+
+            "</div>" + //tides
+
+            "</div>" + //wrapper
+
+            "<div class='error'></div>";
 
 
         //DONT KNOW WHAT THESE INPUT BOXES ARE FOR, MAY NOT NEED THEM WITH AUTFILL DATA FROM BOM"<input type='text' name='text3'/>" +
